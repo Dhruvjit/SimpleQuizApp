7215739b918e401ea4865bfc47ff733c376141d4
DhruBuntu <dhruvjit@desk-now.com>
2023-10-10 23:29:45 +0530

    added two questions, commons, and more flags to the quiz app

---
+++ app/src/main/java/com/tutorials/myapplication/Constants.kt
@@ -0,0 +1,33 @@
+package com.tutorials.myapplication
+
+object Constants {
+    fun getQuestions():ArrayList<Question>{
+        val questionsList = ArrayList<Question>()
+        val que1 = Question(
+            1,
+            "what country does this flag belongs to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina",
+            "Australia",
+            "India",
+            "Austria",
+            1
+
+        )
+
+        /* que 2 */
+        val que2 = Question(
+            2,
+            "what country does this flag belongs to?",
+            R.drawable.ic_flag_of_india,
+            "Brazil",
+            "Germany",
+            "India",
+            "Fiji",
+            3
+
+        )
+        questionsList.add(que2)
+        return questionsList
+    }
+}
--- app/src/main/java/com/tutorials/myapplication/MainActivity.kt
+++ app/src/main/java/com/tutorials/myapplication/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.tutorials.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnStart:Button = findViewById(R.id.btn_start)
+        val etName:EditText = findViewById(R.id.et_name)
+        btnStart.setOnClickListener {
+            if(etName.text.isEmpty()){
+                Toast.makeText(this, "Please enter your name, it cannot be empty", Toast.LENGTH_LONG).show()
+            }else{
+                /* create intent that lets us move from current activity to the next quiz activity */
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                /* start the next activity but do not close the previous one */
+                startActivity(intent)
+                /* finish the current activity or screen and don't let user navigate back by using back button */
+                finish()
+            }
+        }
     }
 }
